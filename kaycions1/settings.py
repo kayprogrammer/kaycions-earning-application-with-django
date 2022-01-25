fbc86d2492cadbab73b01456ceb280e0bf318cf4
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-01-25 20:26:51 +0100

    Add files via upload

--- kaycions1/settings.py
+++ kaycions1/settings.py
@@ -63,6 +63,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'kaycions1.urls'
 
 TEMPLATES = [
