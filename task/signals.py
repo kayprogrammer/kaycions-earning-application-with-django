6479da81f29f185e8f259bb2cb1b9e062a60a723
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-01-30 01:36:23 +0100

    Update signals.py

--- task/signals.py
+++ task/signals.py
@@ -1,16 +1,17 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-from django.contrib.auth.models import User, Group
 from . models import Worker
+from django.contrib.auth.models import User, Group
 
 def worker_profile(sender, instance, created, **kwargs):
     if created:
         group = Group.objects.get(name='worker')
         instance.groups.add(group)
-
-        Worker.objects.create(user=instance, full_name=instance.full_name, email=instance.email)
+        if instance.referral_code != "":
+            Worker.objects.create(user=instance, full_name=instance.full_name, email=instance.email, recommended_by = Worker.objects.get(code=instance.referral_code).user)
+        else:
+            Worker.objects.create(user=instance, full_name=instance.full_name, email=instance.email, recommended_by = None)
         print('Profile Created!')
 
 post_save.connect(worker_profile, sender=settings.AUTH_USER_MODEL)
