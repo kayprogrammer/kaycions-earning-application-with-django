bcf161426701221041925bd413bcd730db378e1e
Kenechi Ifeanyi <kenechiifeanyi@gmail.com>
2022-07-23 15:25:13 +0100

    Update settings.py

--- kaycions1/settings.py
+++ kaycions1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'qhgef*-7ix3$y33dr6x^&pp$n9=9s^wa5kvw3&vjtq_cyg2x)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
